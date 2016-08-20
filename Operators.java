ab0ed568655301de37de894742bd642f1ec73976
Anurag Choudhary <anu5ns14@gmail.com>
2016-08-20 17:20:38 +0530

    Operators assignment
    Operators assignment

---
+++ Operators.java
@@ -0,0 +1,24 @@
+public class Operators {
+	public static void main(String args[]){
+		int x = 5 , y =10;
+		
+		int expOne = x + y *2;
+		int expTwo = x -y + 2;
+		int expThree = (x+y)* 2;
+		int expFour = y%4;
+		
+		System.out.println("Out of first expression is "+expOne);
+		System.out.println("Out of second expression is "+expTwo);
+		System.out.println("Out of third expression is "+expThree);
+		System.out.println("Out of fourth expression is "+expFour);
+		
+	}
+}
+
+
+/* 
+*Out of first expression is 25
+*Out of second expression is -3
+*Out of third expression is 30
+*Out of fourth expression is 2
+*/
